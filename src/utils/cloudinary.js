7268b858849a5ddbf4e57cb26a3c1f72091bf3c0
Tejas <tejashinde0612@gmail.com>
2024-02-12 12:19:41 +0530

    add register controller & bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,11 +5,11 @@ cloudinary.config({
 
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET
 
 });
 
-const UploadOnCloudinary=async(localFilePath)=>{
+const uploadOnCloudinary=async(localFilePath)=>{
     try {
         if(!localFilePath) return null;
         //upload the file on the cloudinary
@@ -24,4 +24,4 @@ const UploadOnCloudinary=async(localFilePath)=>{
     }
 }
 
-export {UploadOnCloudinary}
+export {uploadOnCloudinary}
